c1538e5a679796b11b343664062a7bde700d2970
Rahul Rai <rairayrahul22@gmail.com>
2026-04-28 00:07:46 +0530

    Imporved progam.cs no need to call any class

--- Program.cs
+++ Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Reflection;
+
+var assembly = Assembly.GetExecutingAssembly();
+
+if (args.Length == 0)
+{
+    var runnableTypes = assembly
+        .GetTypes()
+        .Where(t => t.IsClass && t.GetMethod("Test", BindingFlags.Public | BindingFlags.Static) != null)
+        .OrderBy(t => t.Name)
+        .ToList();
+
+    if (runnableTypes.Count == 0)
+    {
+        Console.WriteLine("No problem classes with a Test() method found.");
+        return;
+    }
+
+    Console.WriteLine($"Found {runnableTypes.Count} problem(s):\n");
+    foreach (var t in runnableTypes)
+        Console.WriteLine($"  {t.Name}");
+
+    Console.WriteLine("\nUsage: dotnet run <ClassName>");
+    return;
+}
+
+string className = args[0];
+
+var type = assembly
+    .GetTypes()
+    .FirstOrDefault(t => t.IsClass && t.Name.Equals(className, StringComparison.OrdinalIgnoreCase));
+
+if (type is null)
+{
+    Console.WriteLine($"Error: No class named '{className}' found.");
+    Console.WriteLine("Run without arguments to list all available problems.");
+    Environment.Exit(1);
+    return;
+}
+
+var testMethod = type.GetMethod("Test", BindingFlags.Public | BindingFlags.Static);
+
+if (testMethod is null)
+{
+    Console.WriteLine($"Error: '{className}' has no public static Test() method.");
+    Console.WriteLine("Add:  public static void Test() { /* test cases */ }");
+    Environment.Exit(1);
+    return;
+}
+
+Console.WriteLine($"Running: {type.Name}\n");
+testMethod.Invoke(null, null);
