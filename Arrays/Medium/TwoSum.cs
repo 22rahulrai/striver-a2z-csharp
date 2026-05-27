6d59f55c69f138486b57dd0fed786e45d49142ca
Rahul Rai <rairayrahul22@gmail.com>
2026-05-28 01:01:23 +0530

    fix

--- Arrays/Medium/TwoSum.cs
+++ Arrays/Medium/TwoSum.cs
@@ -123,7 +123,7 @@ public class TwoSum {
 
     public static void Test()
     {
-        (int[] input, int []expected)[] cases =
+        (int[] input, int target,int []expected)[] cases =
         [
             (new int[]{0,1,0,3,12},  new int[]{1,3,12,0,0}),
             (new int[]{1,0,1},  new int[]{1,1,0}),
