b11d1e38325e364e628eec5c73bf8be4eac72e61
Rahul Rai <rairayrahul22@gmail.com>
2026-05-28 01:02:07 +0530

    fix

--- Arrays/Medium/TwoSum.cs
+++ Arrays/Medium/TwoSum.cs
@@ -125,12 +125,11 @@ public class TwoSum {
     {
         (int[] input, int target,int []expected)[] cases =
         [
-            (new int[]{0,1,0,3,12},  new int[]{1,3,12,0,0}),
-            (new int[]{1,0,1},  new int[]{1,1,0}),
-            (new int[]{1 ,0 ,2 ,3 ,0 ,4 ,0 ,1}, new int[]{1 ,2 ,3 ,4 ,1 ,0 ,0 ,0}),
-            (new int[]{1,2,0,1,0,4,0}, new int[]{1,2,1,4,0,0,0}),
-            (new int[]{0},  new int[]{0}),
-            (new int[]{2,1},  new int[]{2,1}),
+            (new int[]{2,7,11,15}, 9, new int[]{0,1}),
+            (new int[]{3,2,4}, 6, new int[]{1,2}),
+            (new int[]{3,3}, 6, new int[]{0,1}),
+            (new int[]{1,5,3,7}, 8, new int[]{0,3}),
+            (new int[]{10,20,30,40}, 50, new int[]{0,3})
         ];
 
         int pass = 0, fail = 0;
