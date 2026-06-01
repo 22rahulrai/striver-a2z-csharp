853080be1979c4436776fadf43ae12dcacac3a04
Rahul Rai <rairayrahul22@gmail.com>
2026-06-01 23:15:47 +0530

    fix

--- Arrays/Medium/MajorityElement.cs
+++ Arrays/Medium/MajorityElement.cs
@@ -19,7 +19,7 @@
  */
 
 public class MajorityElement {
-    public static int Approach_one(int[] nums) {
+    public static int Approach_Two(int[] nums) {
         Dictionary<int,int> map = new Dictionary<int,int>();
 
         foreach(int n in nums){
