1b928400c4e0499d06d0614ee8c10c2fa6fc73e1
Rahul Rai <rairayrahul22@gmail.com>
2026-06-08 01:18:54 +0530

    8-June

--- Arrays/Medium/ArrayLeaders.cs
+++ Arrays/Medium/ArrayLeaders.cs
@@ -56,6 +56,9 @@ public class ArrayLeaders {
     {
 
         int n = arr.Length;
+
+        if (arr == null || n == 0)
+            return new List<int>();
         
         List<int> res= new List<int>();
         res.Add(arr[n-1]);
