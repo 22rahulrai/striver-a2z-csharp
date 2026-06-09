35d4011abb22777f23e7891ea59ced44a23df68d
Rahul Rai <rairayrahul22@gmail.com>
2026-06-10 00:08:57 +0530

    fix

--- Arrays/Hard/ThreeSum.cs
+++ Arrays/Hard/ThreeSum.cs
@@ -2,8 +2,8 @@
  * Problem   : 3Sum
  * Link      : https://leetcode.com/problems/3sum/
  * Platform  : LeetCode
- * Difficulty: Hard
- * Topic     : Arrays
+ * Difficulty: Medium
+ * Topic     : Arrays, Two Pointers, Sorting
  * Date      : 2026-06-09
  *
  * Approach:
