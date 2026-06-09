5029e7c6fc343bf5dfcae5ecbb35d66d216aa704
Rahul Rai <rairayrahul22@gmail.com>
2026-06-10 00:05:55 +0530

    fix

--- Arrays/Hard/ThreeSum.cs
+++ Arrays/Hard/ThreeSum.cs
@@ -39,7 +39,7 @@ public class ThreeSum {
         
         for(int i = 0; i < n; i++){
             for(int j = i+1; j < n; j++){
-                for(int k =j+1; j < n; j++)
+                for(int k = j + 1; k < n; k++)
                 {
                     if(arr[i] + arr[j] + arr[k] == 0)
                     {
