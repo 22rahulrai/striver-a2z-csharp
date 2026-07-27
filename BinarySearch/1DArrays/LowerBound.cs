53fe0244cd659380be4bad88329c419b5652dfec
Rahul Rai <rairayrahul22@gmail.com>
2026-07-27 21:45:23 +0530

    fix

--- BinarySearch/1DArrays/LowerBound.cs
+++ BinarySearch/1DArrays/LowerBound.cs
@@ -7,3 +7,4 @@
  * Definition:
  *   Floor of x = the largest element in the array that is less than or equal to x.
  *   Returns the index of the floor element.
+ *   If no floor exists, returns -1.
