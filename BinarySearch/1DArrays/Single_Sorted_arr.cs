346af4f4bb55d708a7bb19eba94519e1eb25a684
Rahul Rai <rairayrahul22@gmail.com>
2026-08-11 11:50:34 +0530

    Add Approach Four using Binary Search to find the single element in a sorted array

--- BinarySearch/1DArrays/Single_Sorted_arr.cs
+++ BinarySearch/1DArrays/Single_Sorted_arr.cs
@@ -90,6 +90,41 @@ public class Single_Sorted_arr
         return nums[nums.Length - 1];
     }
 
-    
+    public static int Approach_Four(int[] nums) //using Binary Search
+    {
+        int n = nums.Length;
+
+        if(n == 1)
+            return nums[0];
+
+        if(nums[0] != nums[1])
+            return nums[0];
+        
+        if(nums[n-1] != nums[n-2])
+            return nums[n-1];
+        
+        int s = 0;
+        int e = nums.Length - 2;
+
+        while(s <= e)
+        {
+            int mid = s + (e-s)/2;
+
+            if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1])
+                return nums[mid];
+
+            if((mid % 2 == 0 && nums[mid] == nums[mid+1]) || (mid % 2 == 1 && nums[mid] == nums[mid - 1]))
+            {
+                s = mid + 1;
+            }
+            else
+            {
+                e = mid - 1;
+            } 
+        }
+
+        return - 1;
+    }
+
     
 }
