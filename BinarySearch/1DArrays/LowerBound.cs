6a4fd37d00a0a42e3e07309bdcdb2c59bb07ea12
Rahul Rai <rairayrahul22@gmail.com>
2026-08-14 22:58:19 +0530

    fixed date

--- BinarySearch/1DArrays/LowerBound.cs
+++ BinarySearch/1DArrays/LowerBound.cs
@@ -2,7 +2,7 @@
  * Problem   : Floor in a Sorted Array
  * Topic     : Binary Search
  * Difficulty: Easy
- * Date      : 2026-07-26
+ * Date      : 2026-08-04
  *
  * Definition:
  *   Floor of x = the largest element in the array that is less than or equal to x.
